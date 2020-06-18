10ccfc066acea876a1c1dd2f0feb010810655b22
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-06-18 16:58:33 +0200

    last commit

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://localhost:4000'
 
 Vue.use(Vuex)
 
